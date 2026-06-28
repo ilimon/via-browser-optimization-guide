88185550159eba09ec81c397788a55e99aa9b372
Noushad M. <53857835+ilimon@users.noreply.github.com>
2026-06-29 00:49:23 +0600

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Close nav when a link is clicked on mobile
+  document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', () => {
+      document.querySelector('.nav-links').classList.remove('open');
+    });
+  });
